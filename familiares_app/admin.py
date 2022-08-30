d0c945a576eed00e96eec28f767fee8970faf3c1
FrancoCubino <francocubino23@gmail.com>
2022-08-29 23:41:56 -0300

    Primer Commit con el server funcionando

---
+++ familiares_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from familiares_app.models import Familiar
+
+# Register your models here.
+admin.site.register(Familiar)
---
+++ familiares_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from .models import Familiar
+
+# Create your views here.
+def home(req):
+    
+    familiares = Familiar.objects.all()
+    
+    return render(req, "index.html",{"familiares": familiares})
